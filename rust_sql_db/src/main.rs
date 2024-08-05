353aaa7a7efbf5c50cf7ef8dda93ba181bf13e4d
Xavier <xavier.ogay@epfl.ch>
2024-08-05 17:44:21 +0200

    Finish draft of the parser and main

--- rust_sql_db/src/main.rs
+++ rust_sql_db/src/main.rs
@@ -1,6 +1,30 @@
 mod tokenizer;
 mod parser;
 
+use std::io::{self, Write};
+use tokenizer::Tokenizer;
+use parser::{Parser, Query};
+
 fn main() {
-    println!("Hello, world!");
+    loop{
+        print!("sql>");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+
+        if input.trim().eq_ignore_ascii_case("exit") {
+            break;
+        }
+
+        let mut tokenizer = Tokenizer::new(input);
+        let tokens = tokenizer.tokenize();
+        println!("Tokens: {:?}", tokens);
+
+        let mut parser = Parser::new(tokens);
+        match parser.parse() {
+            Ok(query) => println!("Parsed Query: {:?}", query),
+            Err(e) => println!("Error {}", e),
+        }
+    }
 }
--- rust_sql_db/src/parser.rs
+++ rust_sql_db/src/parser.rs
@@ -43,6 +43,20 @@ impl Parser {
         Ok(Query::Select {columns, table, condition})
     }
 
+    fn parse_insert(&mut self) -> Result<Query, String> {
+        self.position += 1; //Skip "INSERT"
+        self.position += 1; //Skip "INTO"
+        let table = self.parse_identifier()?;
+        self.position += 1; //Skip "("
+        let columns = self.parse_columns()?;
+        self.position += 1; //Skip ")"
+        self.position += 1; //Skip "VALUES"
+        self.position += 1; //Skip "("
+        let values = self.parse_values()?;
+        self.position += 1; //Skip ")"
+        Ok(Query::Insert { table, columns, values})
+    }
+
     fn parse_update(&mut self) -> Result<Query, String> {
         self.position += 1; //Skip "UPDATE"
         let table = self.parse_identifier()?;
@@ -58,4 +72,80 @@ impl Parser {
         Ok(Query::Update {table, assignments, condition})
     }
 
+    fn parse_delete(&mut self) -> Result<Query, String> {
+        self.position += 1; //Skipp "DELETE"
+        self.position += 1; //Skip "FROM"
+        let table = self.parse_identifier()?;
+        let condition = if self.position < self.tokens.len()
+                            && self.tokens[self.position] == Token::Where {
+            self.position += 1; //Skip "WHERE"
+            Some(self.parse_identifier()?)
+        } else {
+            None
+        };
+        Ok(Query::Delete { table, condition})
+    }
+
+    fn parse_columns(&mut self) -> Result<Vec<String>, String> {
+        let mut columns = Vec::new();
+        while self.position < self.tokens.len() {
+            if let Token::Identifier(ref col) = self.tokens[self.position] {
+                columns.push(col.clone());
+            } else {
+                break;
+            }
+            self.position += 1;
+            if self.position < self.tokens.len() && self.tokens[self.position] == Token::Comma {
+                self.position += 1; //Skip ","
+            } else {
+                break
+            }
+        }
+        Ok(columns)
+    }
+
+    fn parse_values(&mut self) -> Result<Vec<String>, String> {
+        let mut values = Vec::new();
+        while self.position < self.tokens.len() {
+            if let Token::Literal(ref val) = self.tokens[self.position] {
+                values.push(val.clone());
+            } else {
+                break;
+            }
+            self.position += 1;
+            if self.position < self.tokens.len() && self.tokens[self.position] == Token::Comma {
+                self.position += 1; //Skip ","
+            } else {
+                break
+            }
+        }
+        Ok(values)
+    }
+
+    fn parse_assignments(&mut self) -> Result<Vec<(String, String)>, String> {
+        let mut assignments = Vec::new();
+        while self.position < self.tokens.len() {
+            if let Token::Identifier(ref col) = self.tokens[self.position] {
+                self.position += 1; //Skip column name
+                self.position += 1; //Skip "="
+                if let Token::Literal(ref val) = self.tokens[self.position] {
+                    assignments.push((col.clone(), val.clone()));
+                } else {
+                    return Err("Expected literal value".to_string());
+                }
+            } else {
+                break;
+            }
+        }
+        Ok(assignments)
+    }
+
+    fn parse_identifier(&mut self) -> Result<String, String> {
+        if let Some(Token::Identifier(ref ident)) = self.tokens.get(self.position) {
+            self.position += 1;
+            Ok(ident.clone())
+        } else {
+            Err("Expected identifier".to_string())
+        }
+    }
 }
